4752f2b0e437fdc097c81697abef1eb55864d59e
Rob Catling <github@rpc-soft.com>
2020-07-16 16:42:25 +0100

    CR-935 added code to delete whole collection

--- src/main/java/uk/gov/ons/ctp/common/cloud/TestCloudDataStore.java
+++ src/main/java/uk/gov/ons/ctp/common/cloud/TestCloudDataStore.java
@@ -1,8 +1,15 @@
 package uk.gov.ons.ctp.common.cloud;
 
+import com.google.api.core.ApiFuture;
+import com.google.cloud.firestore.CollectionReference;
+import com.google.cloud.firestore.Firestore;
+import com.google.cloud.firestore.FirestoreOptions;
+import com.google.cloud.firestore.QueryDocumentSnapshot;
+import com.google.cloud.firestore.QuerySnapshot;
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
+import javax.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import uk.gov.ons.ctp.common.error.CTPException;
@@ -11,8 +18,29 @@ import uk.gov.ons.ctp.common.firestore.FirestoreWait;
 /** Access the Cloud data store for testing purposes. */
 @Component
 public class TestCloudDataStore implements CloudDataStore {
+  private static final int DELETION_BATCH_SIZE = 100;
 
   @Autowired private FirestoreDataStore dataStore;
+
+  private Firestore firestore;
+
+  @PostConstruct
+  public void create() {
+    firestore = FirestoreOptions.getDefaultInstance().getService();
+  }
+
+  public long deleteCollection(String schema) {
+    CollectionReference collection = firestore.collection(schema);
+
+    long totalDeleted = 0;
+    int batchDeleted = 0;
+    do {
+      batchDeleted = deleteBatch(collection);
+      totalDeleted += batchDeleted;
+    } while (batchDeleted >= DELETION_BATCH_SIZE);
+
+    return totalDeleted;
+  }
 
   @Override
   public void deleteObject(String schema, String key) throws CTPException {
@@ -56,5 +84,21 @@ public class TestCloudDataStore implements CloudDataStore {
         FirestoreWait.builder().collection(collection).key(key).timeout(timeoutMillis).build();
 
     return firestore.waitForObject() != null;
+  }
+
+  // there is no firestore method to delete a collection, so we delete in batches.
+  private int deleteBatch(CollectionReference collection) {
+    int deleted = 0;
+    try {
+      ApiFuture<QuerySnapshot> future = collection.limit(DELETION_BATCH_SIZE).get();
+      List<QueryDocumentSnapshot> documents = future.get().getDocuments();
+      for (QueryDocumentSnapshot document : documents) {
+        document.getReference().delete();
+        ++deleted;
+      }
+    } catch (Exception e) {
+      throw new RuntimeException(e);
+    }
+    return deleted;
   }
 }
